c97b4bd3afbd2b88ea60e93a22271a8cae0e7531
xnomle <Nick.f@hotmail.co.uk>
2024-08-15 19:47:12 +1000

    small updates
    AWS Region,
    Rename SSH Key
    Reduce Instance size
    Removed ReadMe

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "my-DevSecOps-Bucket-NF"
+    region         = "ap-southeast-2"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
