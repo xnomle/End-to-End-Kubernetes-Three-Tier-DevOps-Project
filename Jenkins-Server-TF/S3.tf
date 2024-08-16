d9fa3bc836f1dc3ffb2d80c92868888edc926944
xnomle <Nick.f@hotmail.co.uk>
2024-08-16 10:25:27 +1000

    S3 and Backend config.

--- Jenkins-Server-TF/S3.tf
+++ Jenkins-Server-TF/S3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-DevSecOps-Bucket-NF"
+    bucket         = "my-DevOps-Bucket-NF"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     region         = "ap-southeast-2"
     encrypt        = true
